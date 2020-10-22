7ee9132731fa1382c6297b6e1ed94d213e4279f3
aeuna <4294psy@naver.com>
2020-10-22 12:08:42 +0900

    Add errorMsg and required in Question

--- src/Routes/Question/QuestionContainer.js
+++ src/Routes/Question/QuestionContainer.js
@@ -2,100 +2,122 @@ import React, { useState } from "react";
 import QuestionPresenter from "./QuestionPresenter";
 import { useQuery, useMutation } from "react-apollo-hooks";
 import { QUESTION_QUERY, QUESTION_MUTATION } from "./QuestionQueries";
+import { toast } from "react-toastify";
 
 export default () => {
-  const { loading, data } = useQuery(QUESTION_QUERY);
-  const [
-    updateAdminInfo,
-    { loading: mutationLoading, error: mutationError },
-  ] = useMutation(QUESTION_MUTATION);
+	const { loading, data } = useQuery(QUESTION_QUERY);
+	const [updateAdminInfo, { loading: mutationLoading, error: mutationError }] = useMutation(
+		QUESTION_MUTATION
+	);
 
-  const [adminState, setAdminState] = useState({
-    email: "",
-    pw: "",
-  });
-  const [questionState, setQuestionState] = useState([
-    {
-      order: "",
-      questionType: "",
-    },
-  ]);
-  const [alreadyGetData, setalreadyGetData] = useState(false);
+	const [adminState, setAdminState] = useState({
+		email: "",
+		pw: "",
+	});
+	const [questionState, setQuestionState] = useState([
+		{
+			order: "",
+			questionType: "",
+		},
+	]);
+	const [alreadyGetData, setalreadyGetData] = useState(false);
 
-  const { email, pw } = adminState;
+	const { email, pw } = adminState;
 
-  const addRow = () => {
-    const newData = {
-      order: questionState[questionState.length - 1].order + 1,
-      questionType: "",
-    };
-    setQuestionState((prevData) => [...prevData, newData]);
-  };
+	const addRow = (e) => {
+		e.preventDefault();
+		const newData = {
+			order: questionState[questionState.length - 1].order + 1,
+			questionType: "",
+		};
+		setQuestionState((prevData) => [...prevData, newData]);
+	};
 
-  const deleteRow = (order) => {
-    setQuestionState(
-      questionState.filter((eachRow) => eachRow.order !== order)
-    );
-  };
+	const deleteRow = (e, order) => {
+		e.preventDefault();
+		setQuestionState(questionState.filter((eachRow) => eachRow.order !== order));
+	};
 
-  const onChange = (e) => {
-    const { value, name } = e.target;
-    setAdminState({
-      ...adminState,
-      [name]: value,
-    });
-  };
+	const onChange = (e) => {
+		const { value, name } = e.target;
+		setAdminState({
+			...adminState,
+			[name]: value,
+		});
+	};
 
-  const onOrderChange = (questionType, e) => {
-    const { value } = e.target;
-    setQuestionState(
-      questionState.map((option) =>
-        option.questionType === questionType
-          ? { ...option, order: Number(value) }
-          : option
-      )
-    );
-  };
+	const onOrderChange = (questionType, e) => {
+		const { value } = e.target;
+		setQuestionState(
+			questionState.map((option) =>
+				option.questionType === questionType ? { ...option, order: Number(value) } : option
+			)
+		);
+	};
 
-  const onSubmit = (e) => {
-    e.preventDefault();
-    updateAdminInfo({
-      variables: {
-        email: adminState.email,
-        pw: adminState.pw,
-        questionTypes: questionState,
-      },
-    });
-  };
+	const onSubmit = async (e) => {
+		e.preventDefault();
+		let orderArr = [];
+		for (const question of questionState) {
+			if (orderArr.includes(question.order)) {
+				toast.error("Question Page : Order values should not be the same.");
+				return;
+			}
+			if (question.order === 0) {
+				toast.error("Question Page : Order values should be bigger than 0.");
+				return;
+			}
+			orderArr.push(question.order);
+		}
 
-  const onTypeChange = (order, e) => {
-    const { value } = e.target;
-    setQuestionState(
-      questionState.map((option) =>
-        option.order === order ? { ...option, questionType: value } : option
-      )
-    );
-  };
+		const {
+			data: { updateSettingQuestion },
+		} = await updateAdminInfo({
+			variables: {
+				email: adminState.email,
+				pw: adminState.pw,
+				questionTypes: questionState,
+			},
+		});
 
-  return (
-    <QuestionPresenter
-      loading={loading}
-      data={data}
-      onChange={onChange}
-      onOrderChange={onOrderChange}
-      onTypeChange={onTypeChange}
-      email={email}
-      pw={pw}
-      setAdminState={setAdminState}
-      setQuestionState={setQuestionState}
-      questionState={questionState}
-      mutationLoading={mutationLoading}
-      mutationError={mutationError}
-      onSubmit={onSubmit}
-      setalreadyGetData={setalreadyGetData}
-      alreadyGetData={alreadyGetData}
-      addRow={addRow}
-      deleteRow={deleteRow}
-    />
-  );
+		if (!updateSettingQuestion || mutationError) {
+			toast.error("Error occured while update data.");
+			return;
+		}
+		if (updateSettingQuestion) {
+			toast.success("Sucessfullly Update Data!");
+			return;
+		}
+	};
+
+	const onTypeChange = (order, e) => {
+		const { value } = e.target;
+		setQuestionState(
+			questionState.map((option) =>
+				option.order === order ? { ...option, questionType: value } : option
+			)
+		);
+	};
+
+	return (
+		<QuestionPresenter
+			loading={loading}
+			data={data}
+			onChange={onChange}
+			onOrderChange={onOrderChange}
+			onTypeChange={onTypeChange}
+			email={email}
+			pw={pw}
+			setAdminState={setAdminState}
+			setQuestionState={setQuestionState}
+			questionState={questionState}
+			mutationLoading={mutationLoading}
+			mutationError={mutationError}
+			onSubmit={onSubmit}
+			setalreadyGetData={setalreadyGetData}
+			alreadyGetData={alreadyGetData}
+			addRow={addRow}
+			deleteRow={deleteRow}
+		/>
+	);
 };
