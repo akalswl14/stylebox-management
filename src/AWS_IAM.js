e5a50e065839433721f457c7dd999b21c520ae9c
Minji Ma <gnkryd12@gmail.com>
2020-10-25 05:04:13 +0900

    Add AWS Setting

---
+++ src/AWS_IAM.js
@@ -0,0 +1,3 @@
+export const IAM_ID = String(process.env.REACT_APP_IAM_ID);
+export const IAM_SECRETKEY = String(process.env.REACT_APP_IAM_SECRETKEY);
+export const BUCKET_NAME = String(process.env.REACT_APP_BUCKET_NAME);
