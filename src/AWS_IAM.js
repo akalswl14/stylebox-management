04964638c2ab7936d1e3256e7e87a69efbe764a9
Minji Ma <gnkryd12@gmail.com>
2020-11-08 01:52:44 +0900

    Update AWS_IAM

--- src/AWS_IAM.js
+++ src/AWS_IAM.js
@@ -1,4 +1,6 @@
 export const IAM_ID = String(process.env.REACT_APP_IAM_ID);
 export const IAM_SECRETKEY = String(process.env.REACT_APP_IAM_SECRETKEY);
 export const BUCKET_NAME = String(process.env.REACT_APP_BUCKET_NAME);
-// export const S3_REGION = String(process.env.REACT_S3_REGION);
+export const S3_REGION = String(process.env.REACT_APP_S3_REGION);
+export const S3_URL =
+  "https://" + BUCKET_NAME + ".s3-" + S3_REGION + ".amazonaws.com/";
