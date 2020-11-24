b9e0f335eeb54c1beff0d90e9d8f94e9e023c423
Minji Ma <gnkryd12@gmail.com>
2020-11-24 16:22:08 +0900

    Update Apollo client

--- src/Apollo/Client.js
+++ src/Apollo/Client.js
@@ -3,8 +3,7 @@ import { API_SERVER } from "../AWS_IAM";
 import { defaults, resolvers } from "./LocalState";
 
 export default new ApolloClient({
-  // uri: API_SERVER,
-  uri: "http://localhost:4000/",
+  uri: API_SERVER,
   clientState: {
     defaults,
     resolvers,
