a12e3f8f3f5faa3d18128585667669c3c44241a4
Minji Ma <gnkryd12@gmail.com>
2020-12-15 10:38:34 +0900

    Code Refactoring

--- src/Routes/ShopList/ShopListPresenter.js
+++ src/Routes/ShopList/ShopListPresenter.js
@@ -213,14 +213,12 @@ export default ({ onSubmit, loading, error, data }) => {
 
   const SearchShopList = (e) => {
     e.preventDefault();
+    let rtnSearchOption = {};
     if (ShopListState.SearchOption.SearchSelectBox === "ShopID") {
       if (isNaN(ShopListState.SearchOption.SearchKeyWord)) {
         toast.error("Id must be a number.");
         return;
       }
-    }
-    let rtnSearchOption = {};
-    if (ShopListState.SearchOption.SearchSelectBox === "ShopID") {
       rtnSearchOption = {
         ...ShopListState.SearchOption,
         SearchShopId: true,
