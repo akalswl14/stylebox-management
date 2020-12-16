f22c731ed172e2d34c77c37e88737f72ec44e1c6
Minji Ma <gnkryd12@gmail.com>
2020-12-16 12:43:00 +0900

    Improve filter function returning nothing issue

--- src/Routes/CreateProduct/BranchManagement/BranchRowData.js
+++ src/Routes/CreateProduct/BranchManagement/BranchRowData.js
@@ -24,15 +24,14 @@ export default ({ data }) => {
     const { name } = e.target;
     if (name === "BranchSelectInput") {
       let isExist = false;
-      const rtnData = ProductInfoState.BranchManagement.value.filter(
-        (eachData) => {
-          if (eachData === Number(data.id)) {
-            isExist = true;
-          } else {
-            return eachData;
-          }
+      const rtnData = [];
+      for (const eachBranch of ProductInfoState.BranchManagement.value) {
+        if (eachBranch === Number(data.id)) {
+          isExist = true;
+        } else {
+          rtnData.push(eachBranch);
         }
-      );
+      }
       if (!isExist) rtnData.push(Number(data.id));
       ProductInfoDispatch({
         type: "UPDATE_BRANCH",
