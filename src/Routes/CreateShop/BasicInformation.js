1233c7ab6369bdff113bf1943c40e584bdf64a40
Minji Ma <gnkryd12@gmail.com>
2020-12-18 01:31:28 +0900

    Update style

--- src/Routes/CreateShop/BasicInformation.js
+++ src/Routes/CreateShop/BasicInformation.js
@@ -10,17 +10,12 @@ import { CHECK_SHOPNAME } from "./CreateShopQueries";
 
 const Table = styled.table`
   border-collapse: collapse;
-  border: 1px solid black;
+  border: 1px solid lightgrey;
   width: 100%;
   text-align: center;
   font-size: 15px;
   tr {
     height: 40px;
-  }
-  tr,
-  td,
-  th {
-    border: ${(props) => props.theme.tableBorder};
   }
   td,
   th {
@@ -30,9 +25,14 @@ const Table = styled.table`
   th {
     background-color: #f2f2f2;
     font-weight: 500;
+    border-right: 0.5px solid black;
   }
   .smallerCell {
     width: 600px;
+  }
+  tbody > tr:nth-child(2n) {
+    border-top: 0.5px solid lightgrey;
+    border-bottom: 0.5px solid lightgrey;
   }
 `;
 
