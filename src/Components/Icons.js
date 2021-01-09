f464d52cf0ea16a39ae28e02f2b43dbc3ef6a7ee
Minji Ma <gnkryd12@gmail.com>
2021-01-10 00:59:43 +0900

    Update OpenPageIcon

--- src/Components/Icons.js
+++ src/Components/Icons.js
@@ -129,8 +129,6 @@ export const OpenPageIcon = ({ size = 20 }) => (
     width={size}
     height={size}
     viewBox="0 0 24 24"
-    fill-rule="evenodd"
-    clip-rule="evenodd"
   >
     <path d="M14 4h-13v18h20v-11h1v12h-22v-20h14v1zm10 5h-1v-6.293l-11.646 11.647-.708-.708 11.647-11.646h-6.293v-1h8v8z" />
   </svg>
